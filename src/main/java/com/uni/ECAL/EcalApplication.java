9492a8edc747d52677c004fc0fe11036e03bb732
dilusha07 <dilushasandaruwani07@gmail.com>
2024-08-10 22:45:28 +0530

    update files

---
+++ src/main/java/com/uni/ECAL/EcalApplication.java
@@ -0,0 +1,13 @@
+package com.uni.ECAL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcalApplication.class, args);
+	}
+
+}
