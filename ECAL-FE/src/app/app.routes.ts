5f6629ca87036b88769b9901e1568cd3e48cbc99
dilusha07 <dilushasandaruwani07@gmail.com>
2024-08-11 14:28:57 +0530

    Project initialization

---
+++ ECAL-FE/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { WelcomepageComponent } from './components/welcomepage/welcomepage.component';
+
+export const routes: Routes = [
+    {path: 'welcome', component:WelcomepageComponent}
+];
